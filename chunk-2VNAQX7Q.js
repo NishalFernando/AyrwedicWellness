98fd6210386c6815fc8038a431e00e160f73933d
Nishal-Fernando <91873624+Nishal-Fernando@users.noreply.github.com>
2026-07-30 23:09:15 +0530

    Auto-generated commit

---
+++ chunk-2VNAQX7Q.js
@@ -0,0 +1,1 @@
+import{$a as s,Ma as n,Na as a,T as o,Xa as l}from"./chunk-Z4SNKZHU.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-blog"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),l(1,"blog works!"),a())}})};export{p as BlogComponent};
